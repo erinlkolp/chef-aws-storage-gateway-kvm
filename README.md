9c6f62469d5800367c56c678037b87ac40e070e6
Erin L. Kolp <erinlkolpfoss@gmail.com>
2025-02-12 10:21:04 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# chef-aws-storage-gateway-kvm-rhel9
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: chef-aws-storage-gateway-kvm-rhel9
+# Recipe:: default
+#
+# Copyright:: 2025, The Authors, All Rights Reserved.
